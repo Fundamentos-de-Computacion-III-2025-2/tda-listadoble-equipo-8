5d8e3c55a9d5bfa75e7a5c5d8be7c91825e48aac
marco <marcosebastian290506@gmail.com>
2025-11-12 21:38:52 -0700

    comit final sebastian  eliminarElemento ybuscarElemento

--- src/ListaDoble.java
+++ src/ListaDoble.java
@@ -19,7 +19,6 @@ public class ListaDoble {
     //Metodo para agregar un NodoDoble al Inicio de la Lista Doble TODO @Rodriguez Andrade Denise
     public void insertarInicio(int dato){
 
-
     }
 
     //Metodo para insertar al Final de la lista doble TODO @Leyva Silva Andres Jovany
@@ -27,11 +26,6 @@ public class ListaDoble {
 
     }
 
-
-    /* Metodo para insertar un elemento suponiendo que la lista está en orden ascendente
-    es decir, se debe comenzar a recorrer la lista e insertar el elemento antes del primer
-    número mayor al elemento que se encuentre en la lista, si no se encuentar un dato mayor
-    se inserta al final  */
     //TODO @Martínez Ruiz Josué Ignacio
     public void insertarEnOrden(int dato){
 
@@ -50,27 +44,59 @@ public class ListaDoble {
         return -1;
     }
 
-    //Eliminar un elemento TODO @Ortega Molina Marco Sebastian
+    //Eliminar un elemento Ortega Molina Marco Sebastian
     public int eliminarElemento(int elemento){
-//si se cambia atte sebas
-        return elemento;
+        if (listaVacia()) {
+            return -1;
+        }
+
+        NodoDoble actual = inicio;
+
+        while (actual != null && actual.dato != elemento) {
+            actual = actual.siguiente;
+        }
+
+        if (actual == null) {
+            return -1;
+        }
+
+        int datoEliminado = actual.dato;
+
+        if (actual == inicio && actual == fin) {
+            inicio = null;
+            fin = null;
+        }
+        else if (actual == inicio) {
+            inicio = inicio.siguiente;
+            inicio.anterior = null;
+        }
+        else if (actual == fin) {
+            fin = fin.anterior;
+            fin.siguiente = null;
+        }
+        else {
+            actual.anterior.siguiente = actual.siguiente;
+            actual.siguiente.anterior = actual.anterior;
+        }
+
+        return datoEliminado;
     }
 
-    //Metodo para buscar un elemento TODO @Ortega Molina Marco Sebastian
+    //Metodo para buscar un elemento Ortega Molina Marco Sebastian
     public boolean buscarElemento(int elemento){
-
+        NodoDoble actual = inicio;
+        while(actual != null){
+            if(actual.dato == elemento){
+                return true;
+            }
+            actual = actual.siguiente;
+        }
         return false;
-
     }
 
     //Imprimir los datos de la lista doble de inicio a fin TODO @Ibarra Padilla Sebastian
     public void mostrarInicioFin(){
-        NodoDoble actual=inicio;
-        System.out.println();
-        while(actual!=null){
-            System.out.print(actual.dato+" --> ");
-            actual = actual.siguiente;
-        }
+
     }
 
     //Imprimir los datos de la lista doble de fin a inicio TODO @Ibarra Padilla Sebastian
--- src/Main.java
+++ src/Main.java
@@ -54,9 +54,33 @@ public class Main {
                             break;
                         case 5: //Eliminar al final TODO @Martínez Ruiz Josué Ignacio
                             break;
-                        case 6: //Eliminar TODO @Ortega Molina Marco Sebastian
+                        case 6: //Eliminar  Molina Marco Sebastian
+                            try {
+                                elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el elemento a eliminar:", "Eliminar Elemento", JOptionPane.QUESTION_MESSAGE));
+                                int Elementoeliminado = lista.eliminarElemento(elemento);
+
+                                if (Elementoeliminado != -1) {
+                                    JOptionPane.showMessageDialog(null, "Elemento " + Elementoeliminado + " eliminado.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Elemento " + elemento + " no encontrado en la lista.", "Error", JOptionPane.ERROR_MESSAGE);
+                                }
+                            } catch (NumberFormatException n) {
+                                JOptionPane.showMessageDialog(null, "Error: Ingrese un número válido.", "Error de Ingreso", JOptionPane.ERROR_MESSAGE);
+                            }
                             break;
-                        case 7: //Buscar elemento TODO @Ortega Molina Marco Sebastian
+                        case 7: //Buscar elemento  Molina Marco Sebastian
+                            try {
+                                elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el elemento a buscar:", "Buscar Elemento", JOptionPane.QUESTION_MESSAGE));
+                                boolean encontrado = lista.buscarElemento(elemento);
+
+                                if (encontrado) {
+                                    JOptionPane.showMessageDialog(null, "Elemento " + elemento + " SÍ se encuentra en la lista.", "Encontrado", JOptionPane.INFORMATION_MESSAGE);
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Elemento " + elemento + " NO se encuentra en la lista.", "No Encontrado", JOptionPane.INFORMATION_MESSAGE);
+                                }
+                            } catch (NumberFormatException n) {
+                                JOptionPane.showMessageDialog(null, "Error: Ingrese un número válido.", "Error de Ingreso", JOptionPane.ERROR_MESSAGE);
+                            }
                             break;
                         case 8: //MostrarLista TODO @Ibarra Padilla Sebastian
                             lista.mostrarInicioFin();
